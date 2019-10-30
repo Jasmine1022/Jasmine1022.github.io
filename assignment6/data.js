184fd775f53d193cd1c2eefb886b15577cd04cae
Jasmine <jinyuyang1022@163.com>
2019-10-30 16:16:44 +0800

    Update data.js

--- assignment6/data.js
+++ assignment6/data.js
@@ -18,15 +18,16 @@ $(document).ready(function(){
     $("button#get_data").click(function() {
         var items = [];
         var i = 0;
-        var airtable_read_endpoint = "https://api.airtable.com/v0/appl9brenWHvcmAo4/%E6%BC%94%E5%87%BA%E4%BF%A1%E6%81%AF?api_key=keyFNbgLbJbzpQY3F";
+        var airtable_read_endpoint = "https://api.airtable.com/v0/appl9brenWHvcmAo4/%E6%BC%94%E5%87%BA%E4%BF%A1%E6%81%AF?api_key=keyFNbgLbJbzpQY3F"
         var dataSet = [];
         $.getJSON(airtable_read_endpoint, function(result) {
                $.each(result.records, function(key,value) {
                    items = [];
                        items.push(value.fields.演出者);
                        items.push(value.fields.首日);
-                       items.push(value.fields.场地);
                        items.push(value.fields.演出名);
+                       items.push(value.fields.场地);
+                       items.push(value.fields.所属);
                        items.push(value.fields.类型);
                        dataSet.push(items);
                        console.log(items);
@@ -41,10 +42,12 @@ $(document).ready(function(){
                        defaultContent:""},
                      { title: "首日",
                          defaultContent:"" },
-                     { title: "场地",
-                       defaultContent:"" },
                      { title: "演出名",
+                       defaultContent:"" },
+                     { title: "场地",
                        defaultContent:""},
+                     { title: "所属场地",
+                         defaultContent:""},
                      { title: "类型",
                          defaultContent:""},
                  ]
