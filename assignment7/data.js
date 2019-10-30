f681a52aa5fe417f3b68434dd324987e5c714b32
Jasmine <jinyuyang1022@163.com>
2019-10-30 15:41:41 +0800

    Update data.js

--- assignment7/data.js
+++ assignment7/data.js
@@ -52,50 +52,49 @@ $(document).ready(function(){
         }); // end .getJSON
      }); // end button
 
-}); // document ready
+     $("button#get_data2").click(function() {
+        var items = [];
+        var i = 0;
+        var airtable_read_endpoint = "https://api.airtable.com/v0/appZYJodqH2yQcXNV/%E6%BC%94%E5%87%BA%E4%BF%A1%E6%81%AF?api_key=keyFNbgLbJbzpQY3F";
+        var dataSet = [];
+        $.getJSON(airtable_read_endpoint, function(result) {
+               $.each(result.records, function(key,value) {
+                   items = [];
+                       items.push(value.fields.演出者);
+                       items.push(value.fields.total_items_by_category);
+                       dataSet.push(items);
+                       console.log(items);
+                }); // end .each
+                console.log(dataSet);
+  
+             $('#table2').DataTable( {
+                 data: dataSet,
+                 retrieve: true,
+                 columns: [
+                     { title: "类型",
+                       defaultContent:""},
+                     { title: "数量",
+                         defaultContent:"" },
+                 ]
+             } );
+  
+             var chart = c3.generate({
+                  data: {
+                      columns: dataSet,
+                      type : 'bar'
+                  },
+                  axis: {
+                    x: {label: '类型'},
+                    y: {label: '# of Items'}
+                  },
+                  bar: {
+                      title: "# of Items by 类型 Category:",
+                  }
+              });
+  
+        }); // end .getJSON
+  
+     }); // end button
 
-$("button#get_data2").click(function() {
-    var items = [];
-    var i = 0;
-    var airtable_read_endpoint = "https://api.airtable.com/v0/appZYJodqH2yQcXNV/%E6%BC%94%E5%87%BA%E4%BF%A1%E6%81%AF?api_key=keyFNbgLbJbzpQY3F";
-    var dataSet = [];
-    $.getJSON(airtable_read_endpoint, function(result) {
-           $.each(result.records, function(key,value) {
-               items = [];
-                   items.push(value.fields.类型);
-                   items.push(value.fields.total_items_by_category);
-                   dataSet.push(items);
-                   console.log(items);
-            }); // end .each
-            console.log(dataSet);
-
-         $('#table2').DataTable( {
-             data: dataSet,
-             retrieve: true,
-             columns: [
-                 { title: "类型",
-                   defaultContent:""},
-                 { title: "Total Amount",
-                     defaultContent:"" },
-             ]
-         } );
-
-         var chart = c3.generate({
-              data: {
-                  columns: dataSet,
-                  type : 'bar'
-              },
-              axis: {
-                x: {label: '类型'},
-                y: {label: '# of Items'}
-              },
-              bar: {
-                  title: "# of Items by 类型 Category:",
-              }
-          });
-
-    }); // end .getJSON
-
- }); // end button
 
 }); // document ready
