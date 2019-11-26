6ab612c179e144711d566231f23fd021017b118a
Jasmine <jinyuyang1022@163.com>
2019-11-26 16:39:47 +0800

    update

--- Final_Project/js/data.js
+++ Final_Project/js/data.js
@@ -1,10 +1,23 @@
 $(document).ready(function(){
 
+    $("button#hide_h2").click(function() {
+        $("h2").hide(500);
+    });
+
+    $("button#show_h2").click(function() {
+        $("h2").show(300);
+    });
+
+    $("button#clear_screen").click(function() {
+        var $x = $("container");
+        $x.empty();
+    });
+
+     $("button#get_data2").click(function() {
       var items = [];
       var i = 0;
       var airtable_read_endpoint = "https://api.airtable.com/v0/appUbctO2Co9tXvAt/Contain?api_key=key4BwmlceROyHV8A";
       var dataSet = [];
-      
       $.getJSON(airtable_read_endpoint, function(result) {
              $.each(result.records, function(key,value) {
                  items = [];
@@ -15,18 +28,25 @@ $(document).ready(function(){
               }); // end .each
               console.log(dataSet);
 
+           $('#table2').DataTable( {
+               data: dataSet,
+               retrieve: true,
+               columns: [
+                   { title: "Name",
+                     defaultContent:""},
+                   { title: "Contain",
+                       defaultContent:"" },
+               ]
+           } );
+
            var chart = c3.generate({
-            bindto:'#aaachart',
                 data: {
                     columns: dataSet,
                     type : 'bar'
                 },
-                tooltip: {
-                    grouped: false 
-                },
                 axis: {
-                  x: {label: 'Name', position: 'outer-center'},
-                  y: {label: 'Contain',position: 'outer-middle'}
+                  x: {label: 'Name'},
+                  y: {label: 'Contain'}
                 },
                 bar: {
                     title: "所有场馆的最多容纳人数:",
@@ -38,4 +58,5 @@ $(document).ready(function(){
    }); // end button
 
 }); // document ready
+
 
