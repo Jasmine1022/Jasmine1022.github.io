9d9b530c9ec3ac2ff5f50f9c3807e1da9b17f4c2
Jasmine <jinyuyang1022@163.com>
2019-11-26 18:42:44 +0800

    update

--- Final_Project/js/data.js
+++ Final_Project/js/data.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
    $("button#get_data1").click(function() {
     var items = [];
     var i = 0;
-    var airtable_read_endpoint = "https://api.airtable.com/v0/appUbctO2Co9tXvAt/Contain?api_key=key4BwmlceROyHV8A";
+    var airtable_read_endpoint = "url https://api.airtable.com/v0/app4dCIRDc3Hn3jOA/500-1499?api_key=key4BwmlceROyHV8A";
     var dataSet = [];
     $.getJSON(airtable_read_endpoint, function(result) {
            $.each(result.records, function(key,value) {
@@ -37,10 +37,86 @@ $(document).ready(function(){
                 x: {label: 'Name'},
                 y: {label: 'Contain'}
               },
-              bar: {
-                  title: "所有场馆的最多容纳人数:",
-              }
           });
+    
+   $("button#get_data2").click(function() {
+    var items = [];
+    var i = 0;
+    var airtable_read_endpoint = "url https://api.airtable.com/v0/app4dCIRDc3Hn3jOA/1500-2999?api_key=key4BwmlceROyHV8A";
+    var dataSet = [];
+    $.getJSON(airtable_read_endpoint, function(result) {
+           $.each(result.records, function(key,value) {
+               items = [];
+                   items.push(value.fields.Name);
+                   items.push(value.fields.Contain);
+                   dataSet.push(items);
+                   console.log(items);
+            }); // end .each
+            console.log(dataSet);
+
+         var chart = c3.generate({
+              data: {
+                  columns: dataSet,
+                  type : 'bar'
+              },
+              axis: {
+                x: {label: 'Name'},
+                y: {label: 'Contain'}
+              },
+          });
+    
+     $("button#get_data3").click(function() {
+    var items = [];
+    var i = 0;
+    var airtable_read_endpoint = "url https://api.airtable.com/v0/app4dCIRDc3Hn3jOA/3000-9999?api_key=key4BwmlceROyHV8A";
+    var dataSet = [];
+    $.getJSON(airtable_read_endpoint, function(result) {
+           $.each(result.records, function(key,value) {
+               items = [];
+                   items.push(value.fields.Name);
+                   items.push(value.fields.Contain);
+                   dataSet.push(items);
+                   console.log(items);
+            }); // end .each
+            console.log(dataSet);
+
+         var chart = c3.generate({
+              data: {
+                  columns: dataSet,
+                  type : 'bar'
+              },
+              axis: {
+                x: {label: 'Name'},
+                y: {label: 'Contain'}
+              },
+          });
+    
+    $("button#get_data4").click(function() {
+    var items = [];
+    var i = 0;
+    var airtable_read_endpoint = "url https://api.airtable.com/v0/app4dCIRDc3Hn3jOA/10000-?api_key=key4BwmlceROyHV8A";
+    var dataSet = [];
+    $.getJSON(airtable_read_endpoint, function(result) {
+           $.each(result.records, function(key,value) {
+               items = [];
+                   items.push(value.fields.Name);
+                   items.push(value.fields.Contain);
+                   dataSet.push(items);
+                   console.log(items);
+            }); // end .each
+            console.log(dataSet);
+
+         var chart = c3.generate({
+              data: {
+                  columns: dataSet,
+                  type : 'bar'
+              },
+              axis: {
+                x: {label: 'Name'},
+                y: {label: 'Contain'}
+              },
+          });
+
 
     }); // end .getJSON
 
