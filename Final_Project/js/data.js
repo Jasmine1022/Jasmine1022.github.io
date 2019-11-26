695f9da66dc59100c91915949ea29e5f8824db89
Jasmine <jinyuyang1022@163.com>
2019-11-26 15:24:53 +0800

    update

--- Final_Project/js/data.js
+++ Final_Project/js/data.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
 
+    $("button#hide_h2").click(function() {
+        $("h2").hide(500);
+    });
+
+    $("button#show_h2").click(function() {
+        $("h2").show(300);
+        $("h2").css("color","blue");
+        $("h2").html("You clicked me hard.");
+    });
+
+    $("button#clear_screen").click(function() {
+        var $x = $("container");
+        $x.empty();
+    });
+
      $("button#ranking").click(function() {
       var items = [];
       var i = 0;
