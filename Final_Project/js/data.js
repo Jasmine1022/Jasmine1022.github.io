90f285e469f6d1999f774c9d29f0b2c6a1dd567a
Jasmine <jinyuyang1022@163.com>
2019-11-26 19:00:45 +0800

    Update data.js

--- Final_Project/js/data.js
+++ Final_Project/js/data.js
@@ -1,44 +1,59 @@
 $(document).ready(function(){
 
-  $("button#hide_h2").click(function() {
-      $("h2").hide(500);
-  });
+    $("button#hide_h2").click(function() {
+        $("h2").hide(500);
+    });
 
-  $("button#show_h2").click(function() {
-      $("h2").show(300);
-  });
+    $("button#show_h2").click(function() {
+        $("h2").show(300);
+        $("h2").css("color","blue");
+        $("h2").html("You clicked me hard.");
+    });
 
-  $("button#clear_screen").click(function() {
-      var $x = $("container");
-      $x.empty();
-  });
+    $("button#clear_screen").click(function() {
+        var $x = $("container");
+        $x.empty();
+    });
 
-   $("button#get_data1").click(function() {
-    var items = [];
-    var i = 0;
-    var airtable_read_endpoint = "url https://api.airtable.com/v0/app4dCIRDc3Hn3jOA/500-1499?api_key=key4BwmlceROyHV8A";
-    var dataSet = [];
-    $.getJSON(airtable_read_endpoint, function(result) {
-           $.each(result.records, function(key,value) {
-               items = [];
-                   items.push(value.fields.Name);
-                   items.push(value.fields.Contain);
-                   dataSet.push(items);
-                   console.log(items);
-            }); // end .each
-            console.log(dataSet);
+     $("button#get_data1").click(function() {
+      var items = [];
+      var i = 0;
+      var airtable_read_endpoint = "https://api.airtable.com/v0/app4dCIRDc3Hn3jOA/500-1499?api_key=key4BwmlceROyHV8A";
+      var dataSet = [];
+      $.getJSON(airtable_read_endpoint, function(result) {
+             $.each(result.records, function(key,value) {
+                 items = [];
+                     items.push(value.fields.Name);
+                     items.push(value.fields.Contain);
+                     dataSet.push(items);
+                     console.log(items);
+              }); // end .each
+              console.log(dataSet);
 
-         var chart = c3.generate({
-              data: {
-                  columns: dataSet,
-                  type : 'bar'
-              },
-              axis: {
-                x: {label: 'Name'},
-                y: {label: 'Contain'}
-              },
-          });
-        }); 
-    }); 
+           $('#table2').DataTable( {
+               data: dataSet,
+               retrieve: true,
+               columns: [
+                   { title: "Name",
+                     defaultContent:""},
+                   { title: "Contain",
+                       defaultContent:"" },
+               ]
+           } );
+
+           var chart = c3.generate({
+                data: {
+                    columns: dataSet,
+                    type : 'bar'
+                },
+                axis: {
+                  x: {label: 'Name'},
+                  y: {label: 'Contain'}
+                },
+            });
+
+      }); // end .getJSON
+
+   }); // end button
 
 }); // document ready
