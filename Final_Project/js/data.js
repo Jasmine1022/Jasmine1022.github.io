0298b2b4e5237e8689365523f15c97d866431790
Jasmine <jinyuyang1022@163.com>
2019-11-29 19:42:52 +0800

    update

--- Final_Project/js/data.js
+++ Final_Project/js/data.js
@@ -143,13 +143,13 @@ $(document).ready(function(){
  $("button#get_data5").click(function() {
     var items = [];
     var i = 0;
-    var airtable_read_endpoint = "https://api.airtable.com/v0/app4dCIRDc3Hn3jOA/Comments-?api_key=key4BwmlceROyHV8A";
+    var airtable_read_endpoint = "https://api.airtable.com/v0/app4dCIRDc3Hn3jOA/Park?api_key=key4BwmlceROyHV8A";
     var dataSet = [];
     $.getJSON(airtable_read_endpoint, function(result) {
            $.each(result.records, function(key,value) {
                items = [];
-                   items.push(value.fields名称);
-                   items.push(value.fields.TripAdvisor点评数);
+                   items.push(value.fields.Name);
+                   items.push(value.fields.TripAdvisorCommentNumber);
                    dataSet.push(items);
                    console.log(items);
             }); // end .each
@@ -161,8 +161,64 @@ $(document).ready(function(){
                   type : 'bar'
               },
               axis: {
-                x: {label: '名称'},
-                y: {label: 'TripAdvisor点评数'}
+                x: {label: 'Name'},
+                y: {label: 'TripAdvisorCommentNumber'}
+              },
+          });
+    }); // end .getJSON
+ }); // end button
+
+ $("button#get_data6").click(function() {
+    var items = [];
+    var i = 0;
+    var airtable_read_endpoint = "https://api.airtable.com/v0/app4dCIRDc3Hn3jOA/Mall?api_key=key4BwmlceROyHV8A";
+    var dataSet = [];
+    $.getJSON(airtable_read_endpoint, function(result) {
+           $.each(result.records, function(key,value) {
+               items = [];
+                   items.push(value.fields.Name);
+                   items.push(value.fields.TripAdvisorCommentNumber);
+                   dataSet.push(items);
+                   console.log(items);
+            }); // end .each
+            console.log(dataSet);
+
+         var chart = c3.generate({
+              data: {
+                  columns: dataSet,
+                  type : 'bar'
+              },
+              axis: {
+                x: {label: 'Name'},
+                y: {label: 'TripAdvisorCommentNumber'}
+              },
+          });
+    }); // end .getJSON
+ }); // end button
+
+ $("button#get_data7").click(function() {
+    var items = [];
+    var i = 0;
+    var airtable_read_endpoint = "https://api.airtable.com/v0/app4dCIRDc3Hn3jOA/Culture?api_key=key4BwmlceROyHV8A";
+    var dataSet = [];
+    $.getJSON(airtable_read_endpoint, function(result) {
+           $.each(result.records, function(key,value) {
+               items = [];
+                   items.push(value.fields.Name);
+                   items.push(value.fields.TripAdvisorCommentNumber);
+                   dataSet.push(items);
+                   console.log(items);
+            }); // end .each
+            console.log(dataSet);
+
+         var chart = c3.generate({
+              data: {
+                  columns: dataSet,
+                  type : 'bar'
+              },
+              axis: {
+                x: {label: 'Name'},
+                y: {label: 'TripAdvisorCommentNumber'}
               },
           });
     }); // end .getJSON
