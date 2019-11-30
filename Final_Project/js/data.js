af6723d3dc179f16a19467cc5bdecfcc31691c5e
Jasmine <jinyuyang1022@163.com>
2019-11-30 22:15:49 +0800

    update

--- Final_Project/js/data.js
+++ Final_Project/js/data.js
@@ -113,44 +113,4 @@ $(document).ready(function(){
     }); // end .getJSON
  }); // end button
 
- $("button#roll_up").click(function() {
-    var items = [];
-    var i = 0;
-    var airtable_read_endpoint = "https://api.airtable.com/v0/app4dCIRDc3Hn3jOA/Comments?api_key=key4BwmlceROyHV8A";
-    var dataSet = [];
-    $.getJSON(airtable_read_endpoint, function(result) {
-           $.each(result.records, function(key,value) {
-               items = [];
-               items.push(value.fields.名称);
-               items.push(value.fields.类型);
-               items.push(value.fields.TripAdvisor点评数);
-               items.push(value.fields.行政区);
-               items.push(value.fields.地址);
-               items.push(value.fields.交通);
-                   dataSet.push(items);
-                   console.log(items);
-            }); // end .each
-            console.log(dataSet);
-
-         $('#table').DataTable( {
-             data: dataSet,
-             retrieve: true,
-             columns: [
-                { title: "名称",
-                  defaultContent:""},
-                { title: "类型",
-                    defaultContent:"" },
-                { title: "TripAdvisor点评数",
-                  defaultContent:"" },
-                { title: "行政区",
-                  defaultContent:""},
-                  { title: "地址",
-                  defaultContent:""},
-                { title: "交通",
-                    defaultContent:""},
-            ]
-        } );
-    }); // end .getJSON
- }); // end button
-
 }); // document ready
